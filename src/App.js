928ca7b77c11759b12bbbe178f9c9ef19ca2e29e
theneeleshwari <theneeleshwari@gmail.com>
2024-11-30 18:09:19 +0530

    test git setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+      
+      <h1>Hello React native</h1>
       <h1>Hello React native</h1>
     </div>
   );
