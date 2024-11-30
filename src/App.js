821137c51bd0a88d05e4e563824b3e079ec96caf
theneeleshwari <theneeleshwari@gmail.com>
2024-11-30 18:25:28 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+      {/* //THIS IT Main app entry point */}
       <h1>Hello React native</h1>
       <h1>Hello React native</h1>
       <h1>Hello React native</h1>
